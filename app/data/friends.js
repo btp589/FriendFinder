13aac1992ca7c8850fbc97d05a215baf42356804
bphan <btp589@gmail.com>
2019-05-20 00:07:50 -0700

    Adding repository

---
+++ app/data/friends.js
@@ -0,0 +1,63 @@
+module.exports = [
+    {
+        "name": "Iron Man",
+        "photo": "https://images.indianexpress.com/2018/04/avengers-iron-man.jpg",
+        "scores": [1, 3, 4, 4, 5, 5, 3, 5, 5, 4]
+    },
+    {
+        "name": "Captain America",
+        "photo": "https://images.indianexpress.com/2018/04/avengers-captain-america.jpg",
+        "scores": [1, 1, 2, 4, 3, 3, 3, 5, 4, 1]
+    },
+    {
+        "name": "Thor",
+        "photo": "https://images.indianexpress.com/2018/04/avengers-thor.jpg",
+        "scores": [1, 4, 2, 4, 2, 2, 4, 5, 2, 3]
+    },
+    {
+        "name": "Doctor Strange",
+        "photo": "https://images.indianexpress.com/2018/04/avengers-doctorstrange.jpg",
+        "scores": [1, 1, 1, 2, 2, 2, 2, 4, 3, 2]
+    },
+    {
+        "name": "Star Lord",
+        "photo": "https://images.indianexpress.com/2018/04/avengers-starlord.jpg",
+        "scores": [2, 1, 1, 2, 2, 1, 1, 4, 2, 3]
+    },
+    {
+        "name": "Black Panther",
+        "photo": "https://images.indianexpress.com/2018/04/avengers-blackpanther.jpg",
+        "scores": [1, 1, 1, 4, 3, 2, 5, 4, 3, 1]
+    },
+    {
+        "name": "Hulk",
+        "photo": "https://images.indianexpress.com/2018/04/avengers-hulk.jpg",
+        "scores": [1, 1, 5, 5, 2, 4, 3, 5, 3, 1]
+    },
+    {
+        "name": "Spiderman",
+        "photo": "https://images.indianexpress.com/2018/04/avengers-spiderman.jpg",
+        "scores": [1, 5, 5, 5, 2, 4, 3, 5, 4, 1]
+    },
+    {
+        "name": "Black Widow",
+        "photo": "https://images.indianexpress.com/2018/04/avengers-blackwidow.jpg",
+        "scores": [1, 5, 5, 1, 5, 5, 1, 5, 5, 1]
+    },
+    {
+        "name": "Bucky",
+        "photo": "https://images.indianexpress.com/2018/04/avengers-bucky.jpg",
+        "scores": [1, 1, 5, 5, 5, 5, 4, 5, 5, 1]
+    },
+    {
+        "name": "Scarlet Witch",
+        "photo": "https://images.indianexpress.com/2018/04/avengers-scarletwitch.jpg",
+        "scores": [1, 1, 3, 3, 4, 5, 2, 5, 5, 1]
+    },
+    {
+        "name": "Vision",
+        "photo": "https://images.indianexpress.com/2018/04/avengers-vision.jpg",
+        "scores": [5, 1, 1, 3, 5, 5, 1, 1, 3, 1]   
+    }
+];
+
